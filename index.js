d8f0dd93ce8e4444585d0bd07db05b62ee7a18f1
Mallipoudy <vencatveer.mallipoudy@ynov.com>
2022-05-30 15:46:51 +0200

    mongodb

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const port = process.env.PORT || 5000
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-http.listen(port)
+const mongoose = require('mongoose');
+
+const mongoDB = "mongodb://localhost:27017/SuiviProjetDev"
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Database Connected...'))
+    .catch(err => console.log('Error connecting database',err));
+
+http.listen(port,()=> { console.log('Connextion listen on 5000')})
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
